8508f65b1f643e6eabd218ce802f867346f2b154
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-09-24 11:16:50 -0500

    Start building out other components

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,189 +1,52 @@
 import React, {Component, useState} from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 
 import client from './api/client';
-import NoteCard from './note-card';
-import {Card} from "react-bootstrap";
 
 function App() {
 
-    const [questions, setQuestions] = useState([]);
-
-    const loadQuestions = async () => {
-        const notations_data = await client.getData('/notations');
-        let notations = notations_data._embedded.notations;
-        let q = notations.map(notation =>
-            <p key={notation._links.self.href}>
-                {notation.description}
-            </p>
-        );
-        setQuestions(q);
-    }
-
-    let content;
-    if (questions.length > 0) {
-        content = <div>{questions}</div>;
-    } else {
-        content =
-            <Card>
-                <p>no questions</p>
-                <p><Button onClick={loadQuestions}>load questions</Button></p>
-            </Card>
-    }
     return (
-        <Container className='p-3'>
-            <Jumbotron>
-                {content}
-            </Jumbotron>
+        <Container>
+            <div className={'p-5 mb-4 bg-dark rounded-3'}>
+                <div className={'container-fluid py-5'}>
+                    <h1 className={'display-5 fw-bold'}>Note Quiz</h1>
+                    <Quiz/>
+                </div>
+            </div>
         </Container>
     );
 
 }
 
-// class App extends Component {
-//
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             notation: {},
-//             notations: [],
-//             users: [],
-//             pageSize: 2,
-//             pageNum: 0,
-//             links: {},
-//             showNoteCard: false
-//         };
-//         this.onNavigate = this.onNavigate.bind(this);
-//         this.onNotationClick = this.onNotationClick.bind(this);
-//     }
-//
-//     componentDidMount() {
-//         this.onNavigate().then(() => {});
-//     }
-//
-//     async onNavigate(href) {
-//         try {
-//             let results = /page=(\d+)/g.exec(href);
-//             let pageNum = (results) ? results[1] : 0;
-//
-//             const notations_data = await client.getData('/notations');
-//             let notations = notations_data._embedded.notations;
-//
-//             const user_data = await client.getData('/users', pageNum, this.state.pageSize);
-//             let users = user_data._embedded.users;
-//             let links = user_data._links;
-//
-//             this.setState({
-//                 notations: notations,
-//                 users: users,
-//                 links: links,
-//                 pageNum: pageNum
-//             });
-//         } catch (err) {
-//             console.error(err);
-//         }
-//     }
-//
-//     onNotationClick(event, target) {
-//         event.preventDefault();
-//         this.setState({
-//             notation: target
-//         });
-//     }
-//
-//     render() {
-//         let notations = this.state.notations.map(notation =>
-//             <li key={notation._links.self.href}>
-//                 <Button href='#'
-//                         onClick={() => {
-//                             this.setState({
-//                                 notation: notation,
-//                                 showNoteCard: true
-//                             });
-//                         }}
-//                 >
-//                     {notation.description}
-//                 </Button>
-//                 {
-//                     (this.state.notation === notation) &&
-//                     <NoteCard notation={this.state.notation}/>
-//                 }
-//             </li>
-//         );
-//         return (
-//             <Container className='p-3'>
-//                 <Jumbotron>
-//                     {/*<h1>Users</h1>*/}
-//                     {/*<UserList users={this.state.users}*/}
-//                     {/*          links={this.state.links}*/}
-//                     {/*          pageSize={this.state.pageSize}*/}
-//                     {/*          onNavigate={this.onNavigate}*/}
-//                     {/*/>*/}
-//                     <ul>{notations}</ul>
-//                 </Jumbotron>
-//             </Container>
-//         )
-//     }
-//
-// }
+function Quiz() {
 
-const Nav = { first: 'first', prev: 'prev', next: 'next', last: 'last' };
+    const [questions, setQuestions] = useState([]);
 
-class UserList extends Component {
+    const loadQuestions = async () => {
+        const data = await client.getData('/notations');
+        let notations = data._embedded.notations;
+        setQuestions(notations.map(n =>
+            <span key={n._links.self.href}>{n.description} </span>
+        ));
+    };
 
-    constructor(props) {
-        super(props);
-        this.handleNav = this.handleNav.bind(this);
-    }
+    let content = (questions.length > 0)
+        ? <div>{questions}</div>
+        : <Card body className={'position-relative'} style={{ width: '18rem', height: '8rem' }}>
+            <Button className={'position-absolute top-50 start-50 translate-middle'}
+                    style={{ width: '17rem', height: '7rem' }}
+                    onClick={loadQuestions}>
+                Get Questions
+            </Button>
+          </Card>;
 
-    handleNav(event, nav) {
-        event.preventDefault();
-        switch (nav) {
-            case Nav.first: this.props.onNavigate(this.props.links.first.href); break;
-            case Nav.prev:  this.props.onNavigate(this.props.links.prev.href); break;
-            case Nav.next:  this.props.onNavigate(this.props.links.next.href); break;
-            case Nav.last:  this.props.onNavigate(this.props.links.last.href); break;
-        }
-    }
-
-    render() {
-        const users = this.props.users.map(user =>
-            <User key={user._links.self.href} user={user}/>
-        );
-
-        const navLinks = [];
-        if (Nav.first in this.props.links) navLinks.push(<button key={Nav.first} onClick={(event) => this.handleNav(event, Nav.first)}>&lt;&lt;</button>);
-        if (Nav.prev  in this.props.links) navLinks.push(<button key={Nav.prev} onClick={(event) => this.handleNav(event, Nav.prev)}>&lt;</button>);
-        if (Nav.next  in this.props.links) navLinks.push(<button key={Nav.next} onClick={(event) => this.handleNav(event, Nav.next)}>&gt;</button>);
-        if (Nav.last  in this.props.links) navLinks.push(<button key={Nav.last} onClick={(event) => this.handleNav(event, Nav.last)}>&gt;&gt;</button>);
-
-        return (
-            <div>
-                <table>
-                    <tbody>
-                    <tr>
-                        <th>Name</th>
-                    </tr>
-                    {users}
-                    </tbody>
-                </table>
-                <p>
-                    {navLinks}
-                </p>
-            </div>
-        );
-    }
-}
-
-function User(props) {
     return (
-        <tr>
-            <td>{props.user.name}</td>
-        </tr>
+        <>{content}</>
     );
+
 }
 
 

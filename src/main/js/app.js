a3bcadee843bc1935f30d1c17f2db19f8177e88a
Brian Ploeckelman <brian.ploeckelman@wisc.edu>
2021-11-11 10:16:43 -0600

    Layout adjustments for adding answer sets

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -32,10 +32,13 @@ function Quiz() {
 
     const [questions, setQuestions] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState(0);
+    // TODO: add state for answer sets to each question
 
     const loadQuestions = async () => {
         const data = await client.getData('/notations');
         let notations = data._embedded.notations;
+        console.log(`Fetched questions:\n${notations.map(n => JSON.stringify(n) + '\n')}`);
+
         setQuestions(notations.map(n =>
             <div key={n._links.self.href}>
                 <Question notation={n}/>
@@ -43,18 +46,31 @@ function Quiz() {
         ));
     };
 
-    let content = (questions.length > 0)
-        ? <Row><div>{questions[currentQuestion]}</div></Row>
-        : <Row>
-            <Card body className={'position-relative'} style={{ width: '18rem', height: '8rem' }}>
-              <Button className={'position-absolute top-50 start-50 translate-middle'}
-                      style={{ width: '17rem', height: '7rem' }}
-                      onClick={loadQuestions}>
-                  Get Questions
-              </Button>
-            </Card>
-          </Row>
+    let content;
+    if (questions.length > 0) {
+        content =
+            <>
+                <Row>
+                    <div>{questions[currentQuestion]}</div>
+                </Row>
+                <Row>
+                    <div>[answers go here]</div>
+                </Row>
+            </>
         ;
+    } else {
+        content =
+            <Row>
+                <Card body className={'position-relative'} style={{ width: '18rem', height: '8rem' }}>
+                    <Button className={'position-absolute top-50 start-50 translate-middle'}
+                            style={{ width: '17rem', height: '7rem' }}
+                            onClick={loadQuestions}>
+                        Get Questions
+                    </Button>
+                </Card>
+            </Row>
+        ;
+    }
 
     return (
         <>
